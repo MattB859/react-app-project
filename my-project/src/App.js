d78316da0c0589996d23a68d22c700846de3500f
MattB859 <mateus756@hotmail.com>
2022-04-06 13:59:14 +0000

    Added customer support component

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,12 +1,17 @@
 import './index.css';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import About from './components/about/About';
+import Support from './components/customer-support/Support';
+
 
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <About/>
+      <Support/>
     </>
   );
 }
