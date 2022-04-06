96301856caff552b1eb656ff8f3caf18f2ac08fa
MattB859 <mateus756@hotmail.com>
2022-04-06 13:24:21 +0000

    Added hero component

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,10 +1,12 @@
 import './index.css';
 import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Hero/>
     </>
   );
 }
