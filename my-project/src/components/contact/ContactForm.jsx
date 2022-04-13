edc4d9b2528d699ea34b5ea07844c0167ab3a3fb
MattB859 <mateus756@hotmail.com>
2022-04-13 16:56:27 +0000

    Add code refactoring to navbar links and contact form

--- my-project/src/components/contact/ContactForm.jsx
+++ my-project/src/components/contact/ContactForm.jsx
@@ -7,6 +7,8 @@ export class ContactForm extends Component {
         this.state = {
             fname: '',
             lname: '',
+            email: '',
+            category: 'website',
             message: '',
         }
     }
@@ -30,45 +32,56 @@ export class ContactForm extends Component {
             message: event.target.value,
         })
     }
-    handleSubmit = (event) => {
-        event.preventDefault();
+    handleEmailChange = (event) => {
+        this.setState({
+            email: event.target.value,
+        })
+    }
+    handleSubmit = () => {
         console.log(this.state)
     }
     render() {
         return (
-            <section className='w-100 h-[800px]'>
-
+            <section name='contact' className='w-100 h-[800px]'>
                 <div className='w-full h-[800px] bg-gray-800/90 absolute'>
                     <div className='w-full h-full object-cover mix-blend-overlay contact'></div>
                 </div>
-
-                <h2 className='relative text-center text-3xl py-8 text-slate-300 uppercase'>Get in Touch</h2>
+                <h2 className='relative text-center text-3xl lg:pt-16 pt-10 pb-12 text-slate-300 uppercase'>Get in Touch</h2>
                 <form onSubmit={this.handleSubmit} className='relative sm:flex-row lg:w-[40%] w-[100%] mx-auto bg-transparent h-[100]my-20 lg:px-10 px-2'>
-                    <div className='w-full p-2 mr-4 rounded-md mb-4'>
-                        <label htmlFor="id-fname">First Name</label>
-                        <input className='border-1 border-black w-[100%] py-2'
-                            value={this.state.fname}
-                            onChange={this.handleFirstNameChange}
-                            id="id-fname"
-                            name="fname"
-                            type="text"
-                            placeholder=' First Name Here..'
-                        />
+                    <div className='grid grid-cols-2 w-full p-2 mr-4 rounded-md mb-4'>
+                        <div className='flex mr-2'>
+                            <input className='rounded-md w-[100%] px-4 py-3'
+                                value={this.state.fname}
+                                onChange={this.handleFirstNameChange}
+                                id="id-fname"
+                                name="fname"
+                                type="text"
+                                placeholder=' First Name'
+                            />
+                        </div>
+                        <div className='flex ml-2'>
+                            <input className='rounded-md w-[100%] px-4 py-3'
+                                value={this.state.lname}
+                                onChange={this.handleLastNameChange}
+                                id="id-lname"
+                                name="lname"
+                                type="text"
+                                placeholder=' Last Name'
+                            />
+                        </div>
                     </div>
-                    <div className='w-full p-2 mr-4 rounded-md mb-4'>
-                        <label htmlFor="id-lname">Last Name</label>
-                        <input className='border-1 border-black w-[100%] py-2'
-                            value={this.state.lname}
-                            onChange={this.handleLastNameChange}
-                            id="id-lname"
-                            name="lname"
+                    <div className='flex w-full p-2 mr-4 rounded-md mb-4'>
+                        <input className='rounded-md w-[100%] py-3 px-4'
+                            value={this.state.email}
+                            onChange={this.handleEmailChange}
+                            id="id-email"
+                            name="email"
                             type="text"
-                            placeholder=' Last Name Here..'
+                            placeholder=' Email Address'
                             />
                     </div>
-                    <div className='w-full p-2 mr-4 rounded-md mb-4'>
-                        <label htmlFor="id-category">Query category</label>
-                        <select className='w-[100%] py-2'
+                    <div className='w-full p-2 mr-4 rounded-md mb-4'>    
+                        <select className='w-[100%] rounded-md py-3 px-4'
                             id="id-category"
                             name="category"
                             value={this.state.category}
@@ -77,23 +90,20 @@ export class ContactForm extends Component {
                             <option value="website">Website issue</option>
                             <option value="order">Order issue</option>
                             <option value="general">General inquiry</option>
-
                         </select>
                     </div>
-                    <div className='w-full p-2 mr-4 rounded-md mb-4'>
-                        <label htmlFor="id-comments">Message</label>
-                        <textarea className='border-1 border-black w-[100%] h-[200px]'
+                    <div className='w-full p-2 mr-4 rounded-md'>
+                        <textarea className='rounded-md w-[100%] h-[200px] py-3 px-4'
                             id="id-message"
                             name="message"
-                            placeholder=' Type Message Here..'
+                            placeholder=' Message'
                             value={this.state.message}
                             onChange={this.handleMessageChange}
                         />
                     </div>
-                    <div className='w-full mr-4 mx-auto'>
+                    <div className='w-full mr-4 mx-auto p-2'>
                         <input className='bg-indigo-600 text-2xl text-white w-[100%] p-3 rounded-md mb-4 cursor-pointer' type="submit" value="Submit message" />   
                     </div>
-                   
                 </form>
             </section>
         )
--- my-project/src/components/navbar/Navbar.jsx
+++ my-project/src/components/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 import { Link, animateScroll as scroll, } from 'react-scroll'
-import { AiOutlineCloudUpload } from 'react-icons/ai';
 import {MenuIcon, XIcon} from '@heroicons/react/outline'
 
 const Navbar = () => {
@@ -9,16 +8,17 @@ const toggleMenuIcon = () => setNav(!nav)
 const handleClose = () => setNav(!nav)
 
 return (
-    <div className='w-screen h-[80px] z-50 bg-zinc-100 fixed drop-shadow-lg'>
-        <div className='px-2 flex justify-between items-center w-full h-full'>
-            <nav className='flex items-center '>
-                <h1 className='text-3xl font-bold sm:text-4xl logo'><span>e</span>CLOUD </h1>
+    <section className='w-screen h-[80px] z-50 bg-zinc-100 fixed drop-shadow-lg'>
+        <div className='px-4 flex justify-between items-center w-full h-full'>
+            <h1 className='text-3xl mr-28  font-bold sm:text-4xl logo'><span>e</span>CLOUD</h1>
+            <nav>
                 <ul className='hidden md:flex'>
                     <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
                     <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
                     <li><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
                     <li><Link to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link></li>
                     <li><Link to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
+                    <li><Link to="contact" smooth={true} offset={-50} duration={500}>Contact</Link></li>
                 </ul>
             </nav>
             <div className='hidden md:flex pr-4'>
@@ -36,12 +36,13 @@ return (
             <li className='border-b-2 border-zinc-300 w-full text-2xl py-2'><Link onClick={handleClose} to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
             <li className='border-b-2 border-zinc-300 w-full text-2xl py-2'><Link onClick={handleClose} to="platforms" smooth={true} offset={-50} duration={500}>Platforms</Link></li>
             <li className='border-b-2 border-zinc-300 w-full text-2xl py-2'><Link onClick={handleClose} to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
+            <li className='border-b-2 border-zinc-300 w-full text-2xl py-2'><Link onClick={handleClose} to="contact" smooth={true} offset={-50} duration={500}>Contact</Link></li>
             <div className='flex flex-col my-4'>
                 <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
                 <button className='px-8 py-3'>Sign Up</button>
             </div>
         </ul>
-    </div>
+    </section>
   );
 };
 
